c381384a074c09422c721dd8513951e4248188da
MeeWeeL <MeeWeeL@yandex.ru>
2022-04-15 23:18:21 +0300

    The start

---
+++ app/src/main/java/com/meeweel/kanban_board/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.meeweel.kanban_board
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
