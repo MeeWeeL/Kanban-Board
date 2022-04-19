a9448748775483dd77ae0329115bc4eb866d65a9
bai_777 <bai_777@mail.ru>
2022-04-19 19:39:42 +0300

    добавил зависимости для navigation component

--- app/src/main/java/com/meeweel/kanban_board/ui/MainActivity.kt
+++ app/src/main/java/com/meeweel/kanban_board/ui/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.meeweel.kanban_board.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.meeweel.kanban_board.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.apply {
+                beginTransaction().replace(
+                    R.id.container, AuthorizationFragment.newInstance()
+                )
+            }
+        }
     }
 }
