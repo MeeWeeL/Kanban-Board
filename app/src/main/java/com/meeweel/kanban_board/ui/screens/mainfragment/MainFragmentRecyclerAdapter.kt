8c86a43d0de7d20f433c8d8473a7d34922c537de
MeeWeeL <MeeWeeL@yandex.ru>
2022-04-27 19:59:41 +0300

    Создал адаптер и вьюмодель для главного экрана

---
+++ app/src/main/java/com/meeweel/kanban_board/ui/screens/mainfragment/MainFragmentRecyclerAdapter.kt
@@ -0,0 +1,45 @@
+package com.meeweel.kanban_board.ui.screens.mainfragment
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.meeweel.kanban_board.databinding.MainScreenRecyclerItemBinding
+import com.meeweel.kanban_board.domain.basemodels.BoardModel
+
+class MainFragmentRecyclerAdapter :
+    RecyclerView.Adapter<MainFragmentRecyclerAdapter.MainViewHolder>() {
+
+    private var dataList: MutableList<BoardModel> = mutableListOf() // Список данных, которые хотим отобразить ресайклером
+    // В нашем случает тут это список досок
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+        val binding = MainScreenRecyclerItemBinding.inflate( // Создает лайаут который нужно заполнить
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return MainViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) { // Вызывает заполнение лайаута
+        holder.bind(dataList[position])
+    }
+
+    override fun getItemCount(): Int { // Порядковый номер в списке
+        return dataList.size
+    }
+
+    inner class MainViewHolder(private val binding: MainScreenRecyclerItemBinding) : // Возвращает заполненный лайаут итема
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(dataList: BoardModel) { // Заполнение лайаута итема
+            binding.apply {
+
+            }
+        }
+    }
+
+    fun setData(data: List<BoardModel>) {
+        dataList = data.toMutableList()
+        notifyDataSetChanged()
+    }
+}
--- app/src/main/java/com/meeweel/kanban_board/ui/screens/mainfragment/MainScreenFragment.kt
+++ app/src/main/java/com/meeweel/kanban_board/ui/screens/mainfragment/MainScreenFragment.kt
@@ -1,22 +1,26 @@
-package com.meeweel.kanban_board.ui.screens
+package com.meeweel.kanban_board.ui.screens.mainfragment
 
 import android.os.Bundle
 import android.view.*
-import android.widget.Toast
-import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.meeweel.kanban_board.R
 import com.meeweel.kanban_board.databinding.FragmentMainScreenBinding
 import com.meeweel.kanban_board.ui.MAIN
-import com.meeweel.kanban_board.ui.MainActivity
 
 class MainScreenFragment : Fragment() {
 
     private var _binding: FragmentMainScreenBinding? = null
     private val binding: FragmentMainScreenBinding
-    get(){
-        return _binding!!
+        get() {
+            return _binding!!
+        }
+
+    private val viewModel: MainFragmentViewModel by lazy { // Вьюмодель
+        ViewModelProvider(this).get(MainFragmentViewModel::class.java) //
     }
+    private val adapter = MainFragmentRecyclerAdapter() // Адаптер
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +32,30 @@ class MainScreenFragment : Fragment() {
         onFabListener()
         binding.leftTopAppBarMainScreenFragment.setNavigationOnClickListener {
             MAIN.navController.navigate(R.id.action_mainScreenFragment_to_copyOfCreateAccountFragment)
+        }
+
+        binding.mainScreenFragmentRecyclerView.adapter =
+            adapter // Приаттачиваем наш адаптер к ресайклеру, чтобы ресайклер знал, что делать
+
+        val observer = Observer<AppState> { a -> // Создаём подписчика и говорим ему что делать если данные обновились
+            renderData(a)
+        }
+        viewModel.getData().observe(viewLifecycleOwner, observer) // Подписываем нашего подписчика на лайвдату из вьюмодели
+        viewModel.getLocalData() // Просим вьюмодель обновить свои данные (На всякий случай)
+    }
+
+    private fun renderData(data: AppState) = when (data) {
+        is AppState.Success -> {
+            val dataList = data.data
+//            binding.loadingLayout.visibility = View.GONE //
+            adapter.setData(dataList)
+        }
+        is AppState.Loading -> {
+//            binding.loadingLayout.visibility = View.VISIBLE //
+        }
+        is AppState.Error -> {
+//            binding.loadingLayout.visibility = View.GONE //
+
         }
     }
 
