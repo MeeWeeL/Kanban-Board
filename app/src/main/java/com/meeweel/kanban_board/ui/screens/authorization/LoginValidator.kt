c22a40a7dd9e787b115bbbc4d771777f9bd6d546
Stepan <bai_777@mail.ru>
2022-05-29 21:39:06 +0300

    создал два класса для проверки ввода логина и пароля

---
+++ app/src/main/java/com/meeweel/kanban_board/ui/screens/authorization/LoginValidator.kt
@@ -0,0 +1,27 @@
+package com.meeweel.kanban_board.ui.screens.authorization
+
+import android.text.Editable
+import android.text.TextWatcher
+import java.util.regex.Pattern
+
+class LoginValidator : TextWatcher {
+    private var isValidLogin = false
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
+
+    override fun afterTextChanged(editableText: Editable?) {
+        isValidLogin = isValidLogin(editableText)
+    }
+
+    companion object {
+        private val PASSWORD_PATTERN = Pattern.compile(
+            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[\$_-])(?=\\S+\$).{20,}\$"
+        )
+
+        fun isValidLogin(password: CharSequence?): Boolean {
+            return password != null && PASSWORD_PATTERN.matcher(password).matches()
+        }
+    }
+}
---
+++ app/src/main/java/com/meeweel/kanban_board/ui/screens/authorization/PasswordValidator.kt
@@ -0,0 +1,27 @@
+package com.meeweel.kanban_board.ui.screens.authorization
+
+import android.text.Editable
+import android.text.TextWatcher
+import java.util.regex.Pattern
+
+class PasswordValidator : TextWatcher {
+    private var isValidPassword = false
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) = Unit
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) = Unit
+
+    override fun afterTextChanged(editableText: Editable?) {
+        isValidPassword = isValidPassword(editableText)
+    }
+
+    companion object {
+        private val PASSWORD_PATTERN = Pattern.compile(
+            "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#\$%^&+=_*])(?=\\S+\$).{12,}\$"
+        )
+
+        fun isValidPassword(password: CharSequence?): Boolean {
+            return password != null && PASSWORD_PATTERN.matcher(password).matches()
+        }
+    }
+}
