695be7df49c3250b8728e2ae55bf4883cef23ffc
bai_777 <bai_777@mail.ru>
2022-06-03 18:02:58 +0300

    сделал выпадание Popup menu по нажатию на бургер в DoneFragment.kt

--- app/src/main/java/com/meeweel/kanban_board/ui/screens/boardscreen/tasklists/done/DoneFragment.kt
+++ app/src/main/java/com/meeweel/kanban_board/ui/screens/boardscreen/tasklists/done/DoneFragment.kt
@@ -1,9 +1,13 @@
 package com.meeweel.kanban_board.ui.screens.boardscreen.tasklists.done
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatImageButton
+import androidx.appcompat.widget.PopupMenu
 import com.meeweel.kanban_board.R
 import com.meeweel.kanban_board.databinding.FragmentListOfTasksBinding
 import com.meeweel.kanban_board.domain.basemodels.BoardModel
@@ -11,6 +15,7 @@ import com.meeweel.kanban_board.domain.basemodels.Status
 import com.meeweel.kanban_board.domain.basemodels.TaskModel
 import com.meeweel.kanban_board.ui.screens.boardscreen.BoardScreenFragmentViewModel
 import com.meeweel.kanban_board.ui.screens.boardscreen.tasklists.BaseBoardScreenFragment
+import com.meeweel.kanban_board.ui.screens.boardscreen.tasklists.OnBurgerClickListener
 
 class DoneFragment(viewModel: BoardScreenFragmentViewModel) : BaseBoardScreenFragment(viewModel) {
 
@@ -31,7 +36,37 @@ class DoneFragment(viewModel: BoardScreenFragmentViewModel) : BaseBoardScreenFra
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        burgerClick()
+    }
 
+    private fun burgerClick() {
+        adapter.setBurgerClickListener(object : OnBurgerClickListener {
+            override fun onBurgerClick(view: AppCompatImageButton) {
+                val popupMenu = PopupMenu(
+                    requireContext(),
+                    view,
+                    Gravity.CENTER
+                )
+                popupMenu.inflate(R.menu.popup_menu)
+                popupMenu.setForceShowIcon(true)
+                popupMenu.setOnMenuItemClickListener {
+                    when (it.itemId) {
+                        R.id.moveTo -> {
+                            Toast.makeText(requireContext(), "moveTo", Toast.LENGTH_SHORT).show()
+                        }
+                        R.id.changePriority -> {
+                            Toast.makeText(requireContext(), "changePriority", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                        R.id.delete -> {
+                            Toast.makeText(requireContext(), "delete", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                    true
+                }
+                popupMenu.show()
+            }
+        })
     }
 
     override fun setAdapter() {
@@ -47,5 +82,10 @@ class DoneFragment(viewModel: BoardScreenFragmentViewModel) : BaseBoardScreenFra
 
     companion object {
         var board: BoardModel? = null
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+        adapter.removeClickListeners()
     }
 }
